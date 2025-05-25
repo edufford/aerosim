52446e6b03ddc6f1b56eb428d92e9ac71f0160c0
Effendi Dufford <effendi@dufford.dev>
2025-05-25 08:49:21 -0700

    Add Zenoh Python middleware interface

--- aerosim-data/src/lib.rs
+++ aerosim-data/src/lib.rs
@@ -95,12 +95,16 @@ fn _aerosim_data(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     #[cfg(feature = "dds")]
     middleware_module.add_class::<middleware::dds::DDSMiddleware>()?;
 
+    middleware_module.add_class::<middleware::zenoh::ZenohMiddleware>()?;
+
     #[cfg(feature = "kafka")]
     middleware_module.add_class::<middleware::kafka::KafkaSerializer>()?;
     #[cfg(feature = "kafka")]
     middleware_module.add_class::<middleware::kafka::BincodeSerializer>()?;
     #[cfg(feature = "dds")]
     middleware_module.add_class::<middleware::dds::DDSSerializer>()?;
+
+    middleware_module.add_class::<middleware::zenoh::ZenohSerializer>()?;
 
     // Add the types submodule to the main module
     m.add_submodule(&middleware_module)?;
--- aerosim-data/src/middleware/zenoh.rs
+++ aerosim-data/src/middleware/zenoh.rs
@@ -6,8 +6,12 @@ use serde::{Deserialize, Serialize};
 use serde_json;
 use tokio::task;
 
-use crate::middleware::{
-    CallbackClosureRaw, Middleware, MiddlewareRaw, Serializer, SerializerEnum,
+use crate::{
+    middleware::{
+        CallbackClosureRaw, Metadata, Middleware, MiddlewareRaw, PyMiddleware, PySerializer,
+        Serializer, SerializerEnum,
+    },
+    types::TimeStamp,
 };
 
 #[pyclass]
@@ -30,12 +34,16 @@ impl Serializer for ZenohSerializer {
 #[pyclass]
 pub struct ZenohMiddleware {
     session: tokio::sync::OnceCell<Arc<zenoh::Session>>,
+    runtime: Arc<tokio::runtime::Runtime>,
 }
 
 impl ZenohMiddleware {
     pub fn new() -> Self {
         ZenohMiddleware {
             session: tokio::sync::OnceCell::new(),
+            runtime: Arc::new(
+                tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime"),
+            ),
         }
     }
 }
@@ -134,5 +142,159 @@ impl MiddlewareRaw for ZenohMiddleware {
 impl Middleware for ZenohMiddleware {
     fn get_serializer(&self) -> SerializerEnum {
         SerializerEnum::from(ZenohSerializer {})
+    }
+}
+
+impl PyMiddleware for ZenohMiddleware {}
+
+#[pymethods]
+impl ZenohMiddleware {
+    #[new]
+    fn pynew(_py: Python) -> PyResult<Self> {
+        Ok(Self::new())
+    }
+
+    #[pyo3(name = "publish")]
+    #[pyo3(signature = (topic, message, timestamp_sim=None))]
+    fn pypublish(
+        &self,
+        py: Python,
+        topic: &str,
+        message: PyObject,
+        timestamp_sim: Option<TimeStamp>,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pypublish_impl(py, topic, message, timestamp_sim))
+    }
+
+    #[pyo3(name = "subscribe")]
+    fn pysubscribe(
+        &self,
+        py: Python,
+        message_type: PyObject,
+        topic: &str,
+        callback: PyObject,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pysubscribe_impl(py, message_type, topic, callback))
+    }
+
+    #[pyo3(name = "subscribe_all")]
+    fn pysubscribe_all(
+        &self,
+        py: Python,
+        message_type: PyObject,
+        topics: Vec<String>,
+        callback: PyObject,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pysubscribe_all_impl(py, message_type, topics, callback))
+    }
+
+    #[pyo3(name = "publish_raw")]
+    fn pypublish_raw(
+        &self,
+        py: Python,
+        message_type: &str,
+        topic: &str,
+        payload: Py<pyo3::types::PyBytes>,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pypublish_raw_impl(py, message_type, topic, payload))
+    }
+
+    #[pyo3(name = "subscribe_raw")]
+    fn pysubscribe_raw(
+        &self,
+        py: Python<'_>,
+        message_type: &str,
+        topic: &str,
+        callback: PyObject,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pysubscribe_raw_impl(py, message_type, topic, callback))
+    }
+
+    #[pyo3(name = "subscribe_all_raw")]
+    fn pysubscribe_all_raw(
+        &self,
+        py: Python,
+        topics: Vec<(String, String)>,
+        callback: PyObject,
+    ) -> PyResult<()> {
+        self.runtime
+            .block_on(self.pysubscribe_all_raw_impl(py, topics, callback))
+    }
+}
+
+impl PySerializer for ZenohSerializer {}
+
+#[pymethods]
+impl ZenohSerializer {
+    #[new]
+    fn pynew(_py: Python) -> PyResult<Self> {
+        Ok(Self {})
+    }
+
+    #[pyo3(name = "serialize_message")]
+    fn pyserialize_message(
+        &self,
+        py: Python<'_>,
+        metadata: Metadata,
+        data: PyObject,
+    ) -> Option<Vec<u8>> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pyserialize_message_impl(py, &serializer, metadata, data)
+    }
+
+    #[pyo3(name = "deserialize_message")]
+    fn pydeserialize_message(
+        &self,
+        py: Python<'_>,
+        message_type: PyObject,
+        payload: &[u8],
+    ) -> Option<(Metadata, PyObject)> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pydeserialize_message_impl(py, &serializer, message_type, payload)
+    }
+
+    #[pyo3(name = "deserialize_metadata")]
+    fn pydeserialize_metadata(&self, py: Python<'_>, payload: &[u8]) -> Option<Metadata> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pydeserialize_metadata_impl(py, &serializer, payload)
+    }
+
+    #[pyo3(name = "deserialize_data")]
+    fn pydeserialize_data(
+        &self,
+        py: Python<'_>,
+        message_type: PyObject,
+        payload: &[u8],
+    ) -> Option<PyObject> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pydeserialize_data_impl(py, &serializer, message_type, payload)
+    }
+
+    #[pyo3(name = "from_json")]
+    fn pyserialize_from_json(
+        &self,
+        py: Python<'_>,
+        type_name: &str,
+        metadata: &Metadata,
+        data: PyObject,
+    ) -> Option<Vec<u8>> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pyserialize_from_json_impl(py, &serializer, type_name, metadata, data)
+    }
+
+    #[pyo3(name = "to_json")]
+    fn pydeserialize_to_json(
+        &self,
+        py: Python<'_>,
+        type_name: &str,
+        payload: &[u8],
+    ) -> Option<PyObject> {
+        let serializer = SerializerEnum::from(ZenohSerializer {});
+        self.pydeserialize_to_json_impl(py, &serializer, type_name, payload)
     }
 }
